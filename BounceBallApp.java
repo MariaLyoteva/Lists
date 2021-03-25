56ddf9d86049e62afc7474a89f43f81f9c91f9ce
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2021-03-26 00:02:04 +0200

    Update BounceBallApp.java

--- BounceBallApp.java
+++ BounceBallApp.java
@@ -1,5 +1,16 @@
 package lists;
 
-public class BounceBallApp {
+import java.awt.*;
+import javax.swing.*;
 
+public class BounceBallApp {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.add(new BallControl());
+		frame.setTitle("Bouncy");
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
